9ad27385a02e92453a23994ac50307ad9166d35c
Chandon Porter <cneilporter94@gmail.com>
2019-10-15 19:32:21 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get '/student' do 
-    erb :index
-  end
+   resources :students, only: :index
 end
